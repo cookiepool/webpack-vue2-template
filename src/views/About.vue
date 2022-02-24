8957a32cdfcedfc8ab17bd036bb6a493ccbd8578
lihao <lihao@cmii.chinamobile.com>
2022-02-24 10:16:08 +0800

    update

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,54 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="text">This is an about page</h1>
+    <el-button type="primary">按钮</el-button>
+
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+    </el-table>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }
+      ]
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+.about {
+  font-size: 24px;
+  .text {
+    color: red;
+  }
+  .el-table__cell {
+    font-size: 30px !important;
+  }
+}
+</style>
